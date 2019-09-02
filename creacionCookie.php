f90591b7f70f0a9f3b25f27d1b98d757a7cfac74
joze-net <50345325+joze-net@users.noreply.github.com>
2019-09-02 15:02:44 -0500

    falto poner el tiempo de duracion de la cookie

--- creacionCookie.php
+++ creacionCookie.php
@@ -1,4 +1,4 @@
 <?php 
-  setcookie("idioma_seleccionado",$_GET["idioma"]);//creamos  la cookie le damos un nombre y luego con get obtenemos el dato que se envio por la URL
-  header("location: almacenaCookie.php");
+  setcookie("idioma_seleccionado",$_GET["idioma"],time()+86400);//creamos  la cookie le damos un nombre y luego con get obtenemos el dato que se envio por la URL
+  header("location: almacenaCookie.php");//REDIRIGIMOS a la pagina donde se trabajara las opciones de pagina segun la opcion elejida
  ?>
